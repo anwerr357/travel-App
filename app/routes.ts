6ec46e09ecaa0fb3f2571d9349c3ae2384def4bc
Anwer Lahami <anwerlahami@gmail.com>
2026-01-02 15:51:01 +0100

    Routing

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,7 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, layout, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+    layout('routes/admin/admin-layout.tsx',[
+        route('Dashboard', 'routes/admin/Dashboard.tsx'),
+    ])
+] satisfies RouteConfig;
